8e8b94a7e9ca04fa3a6cc9048b0b32ec864dc7f5
printezisn <printezisn@gmail.com>
2024-06-17 01:49:51 +0300

    Add theme switch

---
+++ src/components/theme-switch.ts
@@ -0,0 +1,26 @@
+const initThemeSwitch = () => {
+  const themeSwitch = document.querySelector(
+    '.theme-switch'
+  ) as HTMLButtonElement;
+
+  let theme = localStorage.getItem('theme');
+  if (!theme) {
+    theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  }
+  if (theme === 'dark') {
+    themeSwitch.ariaChecked = 'true';
+  }
+
+  themeSwitch.addEventListener('click', () => {
+    themeSwitch.ariaChecked =
+      themeSwitch.ariaChecked === 'true' ? 'false' : 'true';
+    localStorage.setItem(
+      'theme',
+      themeSwitch.ariaChecked === 'true' ? 'dark' : 'light'
+    );
+  });
+};
+
+export default initThemeSwitch;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import initThemeSwitch from './components/theme-switch';
 import './styles/main.scss';
 
+initThemeSwitch();
+
+// Remove curtain
+setTimeout(() => {
+  document.body.style.opacity = '';
+}, 500);
+
+// Register service worker
 if (import.meta.env.PROD && 'serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sudoku/service-worker.js');
 }
