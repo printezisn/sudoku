1ba169a286d43676a3a1508c118646e5f5d0e6bd
printezisn <printezisn@gmail.com>
2024-06-23 14:06:36 +0300

    Add theme switch template in component file

--- src/components/theme-switch/index.ts
+++ src/components/theme-switch/index.ts
@@ -1,10 +1,5 @@
-import template from './template.html?raw';
 import SunIcon from 'feather-icons/dist/icons/sun.svg?raw';
 import MoonIcon from 'feather-icons/dist/icons/moon.svg?raw';
-
-const componentTemplate = template
-  .replace('{sun}', SunIcon)
-  .replace('{moon}', MoonIcon);
 
 class ThemeSwitch extends HTMLElement {
   private switch: HTMLButtonElement | null = null;
@@ -26,21 +21,23 @@ class ThemeSwitch extends HTMLElement {
   };
 
   connectedCallback() {
-    this.innerHTML = componentTemplate;
-
-    this.switch = this.querySelector('button') as HTMLButtonElement;
-
     let theme = localStorage.getItem('theme');
     if (!theme) {
       theme = window.matchMedia('(prefers-color-scheme: dark)').matches
         ? 'dark'
         : 'light';
     }
-    if (theme === 'dark') {
-      this.switch.ariaChecked = 'true';
-    }
+
+    this.switch = document.createElement('button');
+    this.switch.type = 'button';
+    this.switch.role = 'switch';
+    this.switch.ariaLabel = 'Enable dark theme';
+    this.switch.ariaChecked = theme === 'dark' ? 'true' : 'false';
+    this.switch.innerHTML = `${SunIcon}${MoonIcon}`;
 
     this.switch.addEventListener('click', this.onSwitchChange);
+
+    this.appendChild(this.switch);
   }
 
   disconnectedCallback() {
