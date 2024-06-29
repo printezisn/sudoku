25d29758b1bc12bc92f1568fce82be9629515b90
printezisn <printezisn@gmail.com>
2024-06-29 11:17:35 +0300

    Add link to github project

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import SudokuCell from './components/sudoku-cell';
 import ThemeSwitch from './components/theme-switch';
 import { initGame } from './stores/game/actions';
 import './styles/main.scss';
+import GithubIcon from 'feather-icons/dist/icons/github.svg?raw';
+
+const icons: { [i: string]: string } = {
+  github: GithubIcon,
+};
+
+const setIcons = () => {
+  Array.from(document.querySelectorAll('[data-icon]')).forEach((el) => {
+    const icon = icons[el.getAttribute('data-icon') ?? ''];
+    el.innerHTML = `${icon}${el.innerHTML}`;
+  });
+};
 
 // Initialize components
 customElements.define('app-theme-switch', ThemeSwitch);
@@ -13,6 +25,7 @@ customElements.define('app-sudoku-cell', SudokuCell);
 
 // Initialize game
 initGame();
+setIcons();
 
 // Remove curtain
 setTimeout(() => {
